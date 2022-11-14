0f91ee07ebe589d6adfe13369a08d35b6977e7b4
Eudritch <hoodgail.ben@gmail.com>
2022-11-14 15:43:59 -0500

    refix ses

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const helpers = require("./utils/helpers");
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-    secret: process.env.DB_SESSION_SECRET,
+    secret: "process.env.DB_SESSION_SECRET",
     cookie: { maxAge: 7200000 },
     resave: false,
     saveUninitialized: true,
